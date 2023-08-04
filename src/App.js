bd018f89c8dca76865ac0f793fe7dd48542a30fb
Nikola Veselinović <nik-ves@hotmail.com>
2023-08-04 17:56:21 +0200

    Removed autocomplete reference

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Fragment, useContext, useState } from "react";
 import GlobalStyles from "./GlobalStyles";
 import { ChampionContext } from "./context/champions-context";
-import Autosuggest from "react-autosuggest";
 
 import Champions from "./data/Champions.json";
 
@@ -21,8 +20,6 @@ function App() {
   return (
     <Fragment>
       <GlobalStyles />
-
-      {/* <Autosuggest suggestions={allChampions} /> */}
 
       <button onClick={getAndSetChampionData}>Test</button>
       <button onClick={generateRandomChampionSkin}>View</button>
